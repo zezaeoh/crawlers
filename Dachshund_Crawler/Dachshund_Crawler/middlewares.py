15ad1776a8dfdd3ba9236593983b71b4328f579c
JeaGeon <resistangce@gmail.com>
2018-08-22 13:23:46 +0900

    add rendering error catch

--- Dachshund_Crawler/Dachshund_Crawler/middlewares.py
+++ Dachshund_Crawler/Dachshund_Crawler/middlewares.py
@@ -1,7 +1,8 @@
-from scrapy import signals
+from scrapy import signals, Request
 from selenium import webdriver
 from scrapy.http import HtmlResponse
 from selenium.webdriver.common.keys import Keys
+from scrapy.exceptions import CloseSpider
 
 import time
 
@@ -11,6 +12,8 @@ class JavaScriptMiddleware(object):
     def from_crawler(cls, crawler):
         # This method is used by Scrapy to create your spiders.
         s = cls()
+        s.retry = 0
+
         s.usr_id = "newsdog620"
         s.pw = "shiba620"
         s.driver = webdriver.PhantomJS()
@@ -42,14 +45,32 @@ class JavaScriptMiddleware(object):
         time.sleep(1)
         body = self.driver.page_source.encode('utf-8')
         print("parsing... " + request.url)
+        self.retry = 0
         return HtmlResponse(self.driver.current_url, body=body, encoding='utf-8', request=request)
 
     def process_response(self, request, response, spider):
         return response
 
     def process_exception(self, request, exception, spider):
-        pass
+        print("rendering error occurred!")
+        print(str(exception))
+        self.retry += 1
+        if self.retry > 10:
+            raise CloseSpider('max retries exceeded!')
+        print("move to next url")
+        if spider.url_list:
+            tmp = spider.url_list.pop(0)
+            next_url = tmp['url']
+            rq = spider.Request(url=next_url, callback=spider.parse_post,
+                                meta={'item': tmp['item'], 'reconnect': False})
+            spider.visited_links.add(next_url)
+            return rq
+        else:
+            spider.i += 1
+            rq = spider.Request(spider.postPage.format(spider.i), dont_filter=True, callback=spider.parse)
+            rq.meta['reconnect'] = False
+            return rq
 
     def spider_closed(self, spider):
-        self.driver.close()
+        self.driver.quit()
         print("driver closed!")
--- Spitz_Crawler/Spitz_Crawler/middlewares.py
+++ Spitz_Crawler/Spitz_Crawler/middlewares.py
@@ -1,6 +1,8 @@
-from scrapy import signals
+from scrapy import signals, Request
 from selenium import webdriver
 from scrapy.http import HtmlResponse
+from scrapy.exceptions import CloseSpider
+
 import time
 
 
@@ -10,6 +12,7 @@ class JavaScriptMiddleware(object):
         # This method is used by Scrapy to create your spiders.
         s = cls()
         s.driver = webdriver.PhantomJS()
+        s.retry = 0
         crawler.signals.connect(s.spider_closed, signal=signals.spider_closed)
         return s
 
@@ -19,14 +22,31 @@ class JavaScriptMiddleware(object):
         time.sleep(1)
         body = self.driver.page_source.encode('utf-8')
         print("parsing... " + request.url)
+        self.retry = 0
         return HtmlResponse(self.driver.current_url, body=body, encoding='utf-8', request=request)
 
     def process_response(self, request, response, spider):
         return response
 
     def process_exception(self, request, exception, spider):
-        pass
+        print("rendering error occurred!")
+        print(str(exception))
+        self.retry += 1
+        if self.retry > 10:
+            raise CloseSpider('max retries exceeded!')
+        print("move to next url")
+        if spider.url_list:
+            tmp = spider.url_list.pop(0)
+            next_url = tmp['url']
+            rq = Request(url=next_url, callback=spider.parse_post,
+                                meta={'item': tmp['item']})
+            spider.visited_links.add(next_url)
+            return rq
+        else:
+            spider.i += 1
+            rq = Request(spider.postPage.format(spider.i), dont_filter=True, callback=spider.parse)
+            return rq
 
     def spider_closed(self, spider):
-        self.driver.close()
+        self.driver.quit()
         print("driver closed!")
